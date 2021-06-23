c1ac11bab46ca529a6010adc2895ff4ad773181c
Manpreet kaur <Manpreetrandhawa38@gmail.com>
2021-06-22 21:50:59 -0700

    shippingpeoject

---
+++ shippingpeoject/src/main/java/com/example/shippingpeoject/ShippingpeojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.shippingpeoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingpeojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingpeojectApplication.class, args);
+	}
+
+}
